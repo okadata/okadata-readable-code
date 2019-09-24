e4258f8b1e5bd49e63f54c97799ed4d3fae3250a
okadata <taiki.okada@justsystems.com>
2019-09-24 13:17:19 +0900

    spec3: テキストファイルを読み込んで、出力する処理を実装

--- dictionary/dictionary/main.cpp
+++ dictionary/dictionary/main.cpp
@@ -4,8 +4,33 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main(int argc, const char * argv[]) {
-    std::cout << "上手" << std::endl;
+    
+    if ( argc != 2 )
+    {
+        // 実行方法は、dictionary 読み込むファイルパス
+        std::cerr << "Usage: dictionary input_file_path" << std::endl;
+        return -1;
+    }
+    
+    std::string xInputPath(argv[1]);
+    std::ifstream xInputStream(xInputPath);
+    if ( xInputStream.fail() )
+    {
+        std::cerr << "Error: read input file failed" << std::endl;
+        return -2;
+    }
+    else
+    {
+        std::string xLineBuf;
+        while ( std::getline(xInputStream, xLineBuf) )
+        {
+            std::cout << xLineBuf << std::endl;
+        }
+    }
+    
     return 0;
 }
